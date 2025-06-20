ee151433ac1d067e97518ce96e399d952fb1998f
= <tarekmohammed022@gmail.com>
2025-06-20 19:51:55 +0300

    add bookly app to flutter porfolio

---
+++ bookly_app/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: SplashView(),
+    );
+  }
+}
+
+class SplashView extends StatelessWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const  Scaffold(
+      body: Center(
+        child: Text('Welcome to Bookly!'),
+      ),
+    );
+  }
+}
